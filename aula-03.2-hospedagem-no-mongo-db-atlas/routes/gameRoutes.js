098cca5910fb11f2d1a4cfed16393a99af5dc9ac
João Alexandre Pinto Camargo <149254552+joaoalexandre77@users.noreply.github.com>
2026-08-25 19:56:29 -0300

    aninhados

---
+++ aula-03.2-hospedagem-no-mongo-db-atlas/routes/gameRoutes.js
@@ -0,0 +1,19 @@
+// Endpoints (rotas) de Games
+import express from 'express';
+const gameRoutes = express.Router();
+// Importando o controller
+import gameController from '../controllers/gameController.js';
+
+// Endpoint (rota) para listar todos os jogos
+gameRoutes.get("/games", gameController.getAllGames)
+
+//Endpoit (rota) para cadastrar um jogo
+gameRoutes.post("/games", gameController.createGame);
+
+gameRoutes.delete("/games/:id", gameController.deleteGame);
+
+gameRoutes.put("/games/:id", gameController.updateGame);
+
+gameRoutes.get("/games/:id", gameController.getOneGame);
+
+export default gameRoutes;
---
+++ aula-03.2-hospedagem-no-mongo-db-atlas/services/gameService.js
@@ -0,0 +1,70 @@
+// Serviços de Games
+// Aqui será inserido os métodos para Ler, cadastrar, Alterar e Excluir games
+
+// Importando o Model
+import Game from "../models/Games.js"
+
+class gameService {
+    // Serviço para ler os jogos
+    async getAll() {
+        // Tentativa da promessa (sucesso)
+        try {
+            //  o método .find() do mongoose busca registros
+            const games = await Game.find()
+            return games
+            // Caso ocorra um erro será executado o catch
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async Create(title, year, platform, price) {
+        try {
+            const newGame = new Game({
+            title,
+            year,
+            platform,
+            price
+            })
+        await newGame.save();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async Delete(id) {
+        try {
+            await Game.findByIdAndDelete(id)
+            //O mpetodo findByIdAndDelete() do mongoose busca um registro pela ID e deleta
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async update(id, title, year, platform, price) {
+        try {
+            await Game.findByIdAndUpdate(id, {
+                title,
+                year,
+                platform,
+                price
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getOne(id) {
+        try {
+            const game = await Game.findOne({_id: id});
+            return game;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+
+// Exportando a classe
+export default new gameService()
+
